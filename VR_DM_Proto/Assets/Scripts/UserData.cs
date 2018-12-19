986fc33d3d0fa4d1246ec2c4a06852f8089b11c2
pasha <pasha.brel@gmail.com>
2018-12-19 23:09:11 +1100

    v0.2
    basic functions for sound , text and image setting. added plugins

---
+++ VR_DM_Proto/Assets/Scripts/UserData.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserData : MonoBehaviour
+{
+    public Texture2D picture;
+    private Image myImage;
+    public AudioClip sound;
+    public string nameText;
+    private AudioSource source;
+    private Text myText;
+
+    void Awake()
+    {
+        myImage = GetComponentInChildren<Image>();
+        source = GetComponent<AudioSource>();
+        myText = GetComponentInChildren<Text>();
+
+    }
+
+    // Use this for initialization
+	void Start ()
+	{
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (Input.GetKeyDown(KeyCode.Space))
+	    {
+	       myImage.sprite= Sprite.Create(picture, Rect.MinMaxRect(0,0,picture.width,picture.height),Vector2.zero );
+           PlaySound();
+           LoadText();
+	    }
+	}
+
+    public void PlaySound()
+    {
+        source.clip = sound;
+        source.Play();
+        
+    }
+
+    public void LoadText()
+    {
+        myText.text = nameText;
+    }
+
+
+}
